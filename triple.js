050fe267ddf0a9e14f064b0ff24ba1151da5b37d
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-09-08 13:32:43 +0600

    double and triple equal

---
+++ triple.js
@@ -0,0 +1,16 @@
+// Always use ===
+// equal comparison does not work for non-primitive
+
+const first = 2;
+const second = 2;
+
+if(first == second ){
+    console.log('Values are equal')
+}
+else{
+    console.log('Values are not equal');
+}
+
+// type coercion
+// type conversion
+// type casting
